5ca6e076b69766e76cf3d44a5e60a4ef1d6d257f
BJRambo <sosifam@070805.co.kr>
2014-11-12 23:48:23 +0900

    기본 설치 모듈 정리

--- config/package.inc.php
+++ config/package.inc.php
@@ -2,6 +2,6 @@
 /* Copyright (C) NAVER <http://www.navercorp.com> */
 /* Copyright (C) SWHITE <http://swhite523.com> */
 
-define('_SWHITE_PACKAGE_', 'XE');
+define('_SWHITE_PACKAGE_', 'SE');
 define('_SWHITE_LOCATION_', 'ko');
 define('_SWHITE_LOCATION_SITE_', 'http://www.swhite523.com/');
